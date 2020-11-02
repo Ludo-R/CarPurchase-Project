dce97bcdaba8cc669d20d1f91e3d88335a86df76
Ludo-R <ludovic.randon@icloud.com>
2020-11-02 10:45:37 +0100

    int replace

--- script-creation-bdd.sql
+++ script-creation-bdd.sql
@@ -5,10 +5,10 @@ USE carpurchase
 
 CREATE TABLE cardata (
 Car_Name VARCHAR(99) COMMENT "nom_voitures"
-,Year INT(4) COMMENT "annee d'achat"
-,Selling_Price INT(50) COMMENT "prix_de_vente actuelle"
-,Present_Price INT(50) COMMENT "prix"
-,Kms_Driven INT(10) COMMENT "Kilometrages"
+,Year VARCHAR(4) COMMENT "annee d'achat"
+,Selling_Price VARCHAR(50) COMMENT "prix_de_vente actuelle"
+,Present_Price VARCHAR(50) COMMENT "prix"
+,Kms_Driven VARCHAR(10) COMMENT "Kilometrages"
 ,Fuel_type VARCHAR(50) COMMENT "type de carburant"
 ,Seller_Type VARCHAR(99) COMMENT "type de vendeur"
 ,Transmission VARCHAR(99) COMMENT "Manuel, auto"
